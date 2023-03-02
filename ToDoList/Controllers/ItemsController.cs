b3feb8457e28a97a8cd86baa7c56d4abf89fff01
Ashe Urban <ashe@goldentongue.com>
2023-03-02 10:34:49 -0800

    Complete lesson 38/45.
    Co-authored-by: Justin Lee <lee.justin001126@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,38 +6,52 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
-      {
-        List<Item> allItems = Item.GetAll();
-        return View(allItems);
-      }
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    //   {
+    //     List<Item> allItems = Item.GetAll();
+    //     return View(allItems);
+    //   }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      return View();
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
+    // [HttpPost("/items")]
+    // public ActionResult Create(string description)
+    // {
+    //   Item myItem = new Item(description);
+    //   return RedirectToAction("Index");
+    // }
+
+    // [HttpPost("/items/delete")]
+    // public ActionResult DeleteAll()
+    // {
+    //   Item.ClearAll();
+    //   return View();
+    // }
+
+    // [HttpGet("/items/{id}")]
+    // public ActionResult Show(int id)
+    // {
+    //   Item foundItem = Item.Find(id);
+    //   return View(foundItem);
+    // }
+
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
 
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
   }
 }
