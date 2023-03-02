14018419cb94e2ca79c80583006736f064d88cf9
Ashe Urban <ashe@goldentongue.com>
2023-03-02 13:57:27 -0800

    Explore C# 9 and 10 features. Includes some notes.
    Co-authored-by: Justin Lee <lee.justin001126@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ToDoList
+// namespace ToDoList;
+// To use as implicit statmenet, make the toDoList Namespace a statment by following up with a semi-colon; see above. 
+//Comment out lines 8 and 9 of program.cs or completely remove brackets encapsulating class Program.
+//Comment in line 5 in csproj.
+namespace ToDoList 
 {
   class Program
   {
